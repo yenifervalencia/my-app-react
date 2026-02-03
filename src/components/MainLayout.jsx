d834111ddde7104e26edb94833253407ef521f11
yenifervalencia <yenifer.v.e2909@gmail.com>
2026-02-03 11:22:38 -0500

    main

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Header } from './Header'
+import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div>
+      <Header></Header>
+      <div className="flex flex-col justify-center items-center p-5 gap-4">
+        <Outlet></Outlet>
+      </div>
+    </div>
   )
 }
